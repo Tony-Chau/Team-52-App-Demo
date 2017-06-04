a3826d624570e7c651da7324d9cea3f7707ceeee
Tony Chau <tonychau923@gmail.com>
2017-06-04 13:05:16 +1000

    Created a database function and splashscreen title should not be visible anymore

---
+++ Truii Demo App/Truii Demo App/DataDB.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Mono.Data.Sqlite;
+using System.IO;
+
+namespace Truii_Demo_App
+{
+    public class DataDB
+    {
+        string docsFolder;
+        string path;
+        SqliteConnection connection;
+        Context context;
+        public DataDB(Context context)
+        {
+            this.context = context;
+            docsFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            path = System.IO.Path.Combine(docsFolder, "DataDB.db");
+            connection = new SqliteConnection("Data Source=" + path);
+        }
+        public async void CreateDatabase()
+        {
+            connection.Open();
+            try
+            {
+                using (var connect = new SqliteConnection(connection))
+                {
+                    await connect.OpenAsync();
+                    using (var command = connect.CreateCommand())
+                    {
+                        string QueryCommand = "CREATE TABLE Data(UserID INTEGER PRIMARY KEY AUTOINCREMENT, DataOne int NOT NULL, DataTwo int NOT NULL, DataThree int NOT NULL)";
+                        command.CommandText = QueryCommand;
+                        command.CommandType = System.Data.CommandType.Text;
+                        await command.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this.context, ex.Message, ToastLength.Short).Show();
+            }
+            connection.Close();
+        }
+
+        public bool checkDb()
+        {
+            return File.Exists(path);
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            connection.Open();
+            try
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "Select * FROM Data";
+                    
+                    var read = command.ExecuteReader();
+                    while (read.Read())
+                    {
+                        count += 1;
+                    }
+                }
+            }catch(Exception ex)
+            {
+            }
+            connection.Close();
+            return count;
+        }
+        public void InserData(int dataOne, int dataTwo, int dataThree)
+        {
+            connection.Open();
+            try
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    string QueryCommand = "INSERT INTO Data(DataOne, DataTwo, DataThree) VALUES(" + dataOne + ", " + dataTwo + ", " + dataThree + ")";
+                    command.CommandText = QueryCommand;
+                    var rowcount = command.ExecuteNonQuery();
+                }
+            }catch (Exception ex)
+            {
+                Toast.MakeText(this.context, ex.Message, ToastLength.Short).Show();
+            }
+            connection.Close();
+        }
+    }
+}
--- Truii Demo App/Truii Demo App/SplashActivity.cs
+++ Truii Demo App/Truii Demo App/SplashActivity.cs
@@ -14,19 +14,19 @@ using System.Threading.Tasks;
 
 namespace Truii_Demo_App
 {
-    [Activity(Label = "Truii", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Truii", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait, Theme = "@android:style/Theme.NoTitleBar.Fullscreen")]
     public class SplashActivity : Activity
     {
         protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView (Resource.Layout.Splash);
-
             await Task.Delay(1000);
-            
+            DataDB db = new DataDB(this);
+            db.CreateDatabase();
             StartActivity(new Intent(Application.Context, typeof(HomeActivity)));
         }
-
+        
         
     }
 }
